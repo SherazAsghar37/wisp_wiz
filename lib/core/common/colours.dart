d9c0bc79483a27dfa3b054e69118552299d7f248
sherazasgher37 <sherazasgher37@gmail.com>
2025-04-13 16:10:26 +0500

    test

--- lib/core/common/colours.dart
+++ lib/core/common/colours.dart
@@ -53,5 +53,5 @@ abstract class Colours {
   static const Color grey = Color(0xFFE0E0E0);
   static const Color softGrey = Color(0xFFF4F4F4);
   static const Color lightGrey = Color(0xFFF9F9F9);
-  static const Color white = Color(0xFFFFFFFF);
+  // static const Color white = Color(0xFFFFFFFF);
 }
